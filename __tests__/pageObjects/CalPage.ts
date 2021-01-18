73292a9bcc52770871604e3617408dcdc161b8ea
Helen Cunningham <hkathleenc@gmail.com>
2021-01-18 15:14:15 -0800

    Added: methods: navigate(), maxWindow(), sleep(). Fields: logo (selector to org logo).

--- __tests__/pageObjects/CalPage.ts
+++ __tests__/pageObjects/CalPage.ts
@@ -22,6 +22,8 @@
 
   export class CalPage extends AutomatedElement {
       url: string = "https://lavamaex.org/ourcalendar";
+      logo: By = By.xpath("//*[@class = 'Mobile-bar-branding-logo']");
+      // Page buttons
       ag_button: AgendaButton;
       pin_button: PinboardButton;
       month_button: MonthlyButton;
@@ -29,5 +31,31 @@
      
       constructor() {
           super();
+      }
+
+      /**
+       * Navigates to LavaMaex's calendar web page. 
+       * Then waits for the organization's logo to load.
+       */
+      async navigate() {
+        await this.driver.get(this.url);
+        await this.driver.wait(
+          until.elementIsEnabled(await this.getElement(this.logo))
+        );
+      }
+      /**
+       * Maximizes the web page.
+       * NOTE: This uses code from the web site:
+       * http://www.software-testing-tutorials-automation.com/2020/05/maximize-and-full-screen-window-using.html
+       */
+      async maxWindow() {
+        this.driver.manage().window().maximize();
+      }
+      /**
+       * Pauses the driver for an amount of time determined by the user
+       * Credit: Steven Cooper (project: g1p1-1)
+       */
+      sleep(ms: number) {
+        return new Promise( resolve => setTimeout(resolve, ms));
       }
   }
