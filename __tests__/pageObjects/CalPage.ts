58d466a1bd6b55351ea26b11087b90eea7eb3e21
Helen Cunningham <hkathleenc@gmail.com>
2021-01-18 14:41:14 -0800

    Added url field to CalPage class.

--- __tests__/pageObjects/CalPage.ts
+++ __tests__/pageObjects/CalPage.ts
@@ -21,6 +21,7 @@
 
 
   export class CalPage extends AutomatedElement {
+      url: string = "https://lavamaex.org/ourcalendar";
       ag_button: AgendaButton;
       pin_button: PinboardButton;
       month_button: MonthlyButton;
