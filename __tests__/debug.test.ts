ddd119fea5d4e36b1ec36165cc08d70507707d4e
Helen Cunningham <hkathleenc@gmail.com>
2021-01-18 15:41:40 -0800

    Added basic test block to confirm that the test file runs and that the driver opens and loads the web page.

--- __tests__/debug.test.ts
+++ __tests__/debug.test.ts
@@ -12,3 +12,19 @@ import {
     MonthlyButton,
     CalPage
 } from "./pageObjects/index"
+
+
+
+describe("Early stages work", () => {
+    const page = new CalPage();
+    beforeEach(async () => {
+        page.navigate();
+    });
+    afterAll(async () => {
+        page.driver.quit();
+    });
+    test("", async () => {
+        await page.maxWindow();
+        await page.sleep(5000);
+    });
+});
