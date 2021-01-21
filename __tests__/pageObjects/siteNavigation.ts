209db6413975074d709c93d2b1d8175da5bf5cd9
Helen Cunningham <hkathleenc@gmail.com>
2021-01-21 15:46:54 -0800

    Added new class, SiteNavigation. This will be used to test navigating to org pages using UI.

---
+++ __tests__/pageObjects/siteNavigation.ts
@@ -0,0 +1,14 @@
+import {
+    Builder,
+    By,
+    Capabilities,
+    until,
+    WebDriver,
+    WebElement,
+  } from "selenium-webdriver";
+
+  import { AutomatedElement } from "./index"
+
+  export class SiteNavigation extends AutomatedElement {
+
+  }
