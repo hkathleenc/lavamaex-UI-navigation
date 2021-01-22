ec369ddf64eff943a2b1bebd23b8f64720bd7460
Helen Cunningham <hkathleenc@gmail.com>
2021-01-21 16:20:31 -0800

    Added first iteration of function to jump through array.

--- __tests__/pageObjects/siteNavigation.ts
+++ __tests__/pageObjects/siteNavigation.ts
@@ -10,5 +10,24 @@ import {
   import { AutomatedElement } from "./index"
 
   export class SiteNavigation extends AutomatedElement {
+      index: number;
+      array: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      constructor() {
+          super();
+      }
+
+        testArray() {
+            this.index = 0;
+            for(let i = 1; i <= 2; i++) {
+                for(let j = 0; j <= 10; j++) {
+                    let a = this.array[this.index % 11];
+                    this.index += i;
+                    let b = this.array[this.index % 11];
+                    console.log( '%i  %i', a, b);
+                }
+                console.log("");
+            }
+        }
+
 
   }
