a4b76e34c22556b092d29f364c8ace0e4ab58171
Helen Cunningham <hkathleenc@gmail.com>
2021-02-08 17:06:13 -0800

    Fixed bug in ui_nav_header.test.ts. Tests will pass when program accepts .json objects with even or odd number of button objects.

--- __tests__/debug.test.ts
+++ __tests__/debug.test.ts
@@ -18,80 +18,115 @@ import {
 } from "./pageObjects/index"
 
 import * as navButtons from "./data/demo_data.json"
+import * as array from "./data/debug_data.json"
 
 
+let dummyElem = {
+    "number": "x",
+    "other" : ""
+}
 
-describe("Early stages work", () => {
+let flag = false;
+let length = array.length;
+if ( (length > 2 ) && ( length % 2 == 0 ) ) {
+    flag = true;
+    array.push(dummyElem);
+    length = array.length;
+    console.log(`"Pushed ${array[length - 1].number} onto array.`)
+}
+let index = 0;
+let prev = 0;
+for(let i = 1; i <= length - 1; i++) {
+    for(let j = 0; j <= length - 1; j++) {
+        // We will navigate from the destination page of the button that was just clicked. 
+        prev = index;
+        // Update the index to find the button that will be clicked next.
+        index += i;
+        // If need to skip, skip
+        if( ( flag ) &&  (( index % length == length - 1 ) ||
+                    ( prev % length == length - 1 ))) { continue }
+        console.log(`${array[prev % length].number} to ${array[index % length].number}`)
+    }
+    console.log("");
+} 
+
+/*
+describe("Header Navigation Buttons Work", () => {
     const page = new SiteNavigation();
     beforeAll(async () => {
+        // Navigate to the destination page of the first button in the array.
         await page.navigate(navButtons[0].destURL);
       // maximize?
     });
     afterAll(async () => {
         page.driver.quit();
     });
-        let index = 0;
-        let array: number[] = [0, 1, 2];
-        // let index = 0;
-        // Checking array indices
-        for(let i = 1; i <= array.length - 1; i++) {
-            for(let j = 0; j <= array.length - 1; j++) {
-                let a = array[index % array.length];
-                index += i;
-                let b = array[index % array.length];
-                console.log( '%i  %i', a, b);
-            }
-            console.log(navButtons[i].buttonName);
-        }
-        // Checking process without automation (string confirmation)
-        
-        console.log("Navigating to destURL of first button");
-        for(let i = 1; i <= navButtons.length - 1; i++) {
-            for(let j = 0; j <= navButtons.length - 1; j++) {
-                prev = index;
-                index += i;
-                console.log(`navigating from ${navButtons[prev % length].destPage} to ${navButtons[index % length].destPage}.`);
-            }
-            console.log("")
-        }
-        let index = 0;
-        let prev = 0;
-        let length = navButtons.length;
-        let curURL;
-        let goalURL: string = "";
-        let navButtonBy: By;
-        let caseString: string = "";
-        console.log("Navigated to destURL of navButtons[0]")
-        for(let i = 1; i <= length - 1; i++) {
-            for(let j = 0; j <= length - 1; j++) {
-                prev = index;
-                index += i;
-                navButtonBy = By.xpath(navButtons[index % length].buttonXPath);
-
-                console.log(`navigating from ${navButtons[prev % length].destPage} to 
-                            ${navButtons[index % length].destPage}.`);
-                
-                caseString = `Click: '${navButtons[index % length].buttonName}' button on 
+    // The following two strings will be used to loop around the array:
+    // The array index of the button we will click. Start at 0.
+    let index = 0;  
+    // The array index of the button whose destination page we navigate from. Start at 0.    
+    let prev = 0;     
+    let length = navButtons.length;     // The number of buttons in the array
+    // The following two strings will be used to compare the expected results and actual results of
+    // a navigation button:
+    let curURL;     // The URL of the page we have navigated to   
+    let goalURL: string = "";   // The URL that clicking of the page we should have navigated to
+    let navButtonBy: By;      // A By to locate the button to be tested 
+    // A string that will describe the test base being executed. This will be populated with 
+    // information from button objects.
+    let caseString: string = "";    
+    // We will loop around the array, clicking buttons and comparing expected results against actual results. 
+    // We will take multiple passes around the array, taking successively larger steps in order to 
+    // click every button from every other button's destination page. 
+    // The "step size" is i.
+    for(let i = 1; i <= length - 1; i++) {
+        for(let j = 0; j <= length - 1; j++) {
+            // We will navigate from the destination page of the button that was just clicked. 
+            prev = index;
+            // Update the index to find the button that will be clicked next.
+            index += i;
+            // Create the By element using the xPath of the button-to-be-clicked.
+            navButtonBy = By.xpath(navButtons[index % length].buttonXPath);
+            // Populate the string describing the test about to be executed. 
+            /* For example, if we are testing navigation from the home to about page using the
+               about button, the case description will be:
+               
+                "Click: 'About' button on 'Home' Page.
+                         Leads to: 'About' Page."
+            */
+/*
+            caseString = `Click: '${navButtons[index % length].buttonName}' button on 
                                 '${navButtons[prev % length].destPage}' Page. 
                                 Leads to: '${navButtons[index % length].destPage}' Page.`;
-
-                test(caseString, async () => {
-                    // Click button:
-                    await page.getElement(navButtonBy);
-                    await page.click(navButtonBy);
-                    // Update comparison URL strings
-                    curURL = await page.driver.getCurrentUrl();
-                    goalURL = navButtons[index % length].destURL;
-                    expect(curURL).toBe(goalURL);
-                });
-                if(curURL != goalURL) {
-                    console.log("Failsafe: navigating to destURL after failed test.")
-                    // navigate to destURL
-                    page.navigate(navButtons[index % length].destURL);
-                }
+            // Run the test.
+            test(caseString, async () => {
+                // Get the button to be clicked, click it, and wait.
+                await page.getElement(navButtonBy);
+                await page.click(navButtonBy);
+                await page.sleep(3000);
+                // Update strings to compare actual vs. expected results
+                // set curURL to the URL we have actually navigated to
+                curURL = await page.driver.getCurrentUrl();
+                // set goalURL to the destination URL of the button we just pushed (where we expected
+                // to navigate)
+                goalURL = navButtons[index % length].destURL;
+                // Compare actual vs. expected URLs.
+                expect(curURL).toBe(goalURL);
+            });
+            // If clicking the button did NOT result in navigating the to correct page, navigate to 
+            // the correct page. 
+            // This means that the next test can run correctly even if the previous test failed.
+            if(curURL != goalURL) {
+                // navigate to the current button's destination URL.
+                page.navigate(navButtons[index % length].destURL);
             }
         }
+    }
+});
 
+*/
+
+/*
     
         let whoWeAre:By = By.xpath("//a[@class='Header-nav-item' and text()='Who We Are']");
         let impact:By = By.xpath("//a[@class='Header-nav-item' and text()='Impact']");
@@ -112,5 +147,4 @@ describe("Early stages work", () => {
         let secondUrl = await page.driver.getCurrentUrl();
         expect(secondUrl).toContain("https://lavamaex.org/who-we-are");
         
-
-});
+*/
--- __tests__/ui_nav_header.test.ts
+++ __tests__/ui_nav_header.test.ts
@@ -76,66 +76,113 @@ import {
 import * as navButtons from "./data/header_data.json"
 
 describe("Header Navigation Buttons Work", () => {
+
     const page = new SiteNavigation();
+
     beforeAll(async () => {
         // Navigate to the destination page of the first button in the array.
         await page.navigate(navButtons[0].destURL);
       // maximize?
     });
+
     afterAll(async () => {
         page.driver.quit();
     });
+    // The algorithm this program uses to click through all buttons without backtracking works
+    // when there are an odd number of buttons to be clicked (the algorithm will work for an 
+    // even-numbered array of size 2). To get around this, if an even number of buttons are 
+    // imported from the .JSON file, a dummy button will be pushed onto the array.
+
+    let dummyButton = {
+        "buttonXPath": "",
+        "destURL": "",
+        "buttonName": "",
+        "destPage": ""
+    }
+    
+    let flag = false // A flag that the array is even- or odd-numbered, to be used later.
+    let length = navButtons.length;  // The number of buttons to be tested
+
+    // If the length of the array is greater than 2 and teh number of buttons it contains is even,
+    // push the dummy button onto the array, set the even/odd flag to true, and re-assign the 
+    // length variable.
+    if ( (length > 2 ) && ( length % 2 == 0 ) ) {
+        flag = true;
+        navButtons.push(dummyButton);
+        length = navButtons.length; 
+    }
+
     // The following two strings will be used to loop around the array:
     // The array index of the button we will click. Start at 0.
     let index = 0;  
     // The array index of the button whose destination page we navigate from. Start at 0.    
     let prev = 0;     
-    let length = navButtons.length;     // The number of buttons in the array
+
     // The following two strings will be used to compare the expected results and actual results of
     // a navigation button:
     let curURL;     // The URL of the page we have navigated to   
     let goalURL: string = "";   // The URL that clicking of the page we should have navigated to
+
     let navButtonBy: By;      // A By to locate the button to be tested 
     // A string that will describe the test base being executed. This will be populated with 
     // information from button objects.
-    let caseString: string = "";    
+
+    let caseString: string = "";   
+    
+
     // We will loop around the array, clicking buttons and comparing expected results against actual results. 
     // We will take multiple passes around the array, taking successively larger steps in order to 
     // click every button from every other button's destination page. 
     // The "step size" is i.
     for(let i = 1; i <= length - 1; i++) {
         for(let j = 0; j <= length - 1; j++) {
+
             // We will navigate from the destination page of the button that was just clicked. 
             prev = index;
             // Update the index to find the button that will be clicked next.
             index += i;
+
+            // If the number of buttons to be tested is even, and either the prev or index variables 
+            // are currently on that dummy variable, do not carry out a test. Continue to the 
+            // next iteration.
+            if( ( flag ) &&  (( index % length == length - 1 ) ||
+                    ( prev % length == length - 1 ))) { continue }
+
+            // Otherwise, continue on with the test.
+
             // Create the By element using the xPath of the button-to-be-clicked.
             navButtonBy = By.xpath(navButtons[index % length].buttonXPath);
+
             // Populate the string describing the test about to be executed. 
             /* For example, if we are testing navigation from the home to about page using the
                about button, the case description will be:
-               
+
                 "Click: 'About' button on 'Home' Page.
                          Leads to: 'About' Page."
             */
             caseString = `Click: '${navButtons[index % length].buttonName}' button on 
                                 '${navButtons[prev % length].destPage}' Page. 
                                 Leads to: '${navButtons[index % length].destPage}' Page.`;
+
             // Run the test.
             test(caseString, async () => {
+
                 // Get the button to be clicked, click it, and wait.
                 await page.getElement(navButtonBy);
                 await page.click(navButtonBy);
                 await page.sleep(3000);
+
                 // Update strings to compare actual vs. expected results
                 // set curURL to the URL we have actually navigated to
                 curURL = await page.driver.getCurrentUrl();
                 // set goalURL to the destination URL of the button we just pushed (where we expected
                 // to navigate)
                 goalURL = navButtons[index % length].destURL;
+
                 // Compare actual vs. expected URLs.
                 expect(curURL).toBe(goalURL);
             });
+
             // If clicking the button did NOT result in navigating the to correct page, navigate to 
             // the correct page. 
             // This means that the next test can run correctly even if the previous test failed.
